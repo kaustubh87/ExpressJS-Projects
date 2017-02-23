0d792f1a5531cd65aa8a4258585baf254e3df77e
kvin1087 <kaustubh87@gmail.com>
2017-02-22 22:26:39 -0600

    Added MongoDb Connection

--- todo-app/app.js
+++ todo-app/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/todo-app';
+
 
 const app = express();
 const port = 4321;
@@ -13,10 +16,21 @@ app.use(express.static(path.join(__dirname,'public')));
 app.set('views' , path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req,res,next) => {
-  res.render('index');
+MongoClient.connect(url, (err, database) =>{
+    console.log('MongoDB Connected...');
+    if(err) throw err;
+
+    db = database;
+    Todos = db.collection('todos');
+
+    app.listen(port, () => {
+      console.log('Server running on port ' +port);
+    });
 });
 
-app.listen(port, () => {
-  console.log('Server running on port ' +port);
+app.get('/', (req,res,next) => {
+  Todos.find({}).toArray((err, todos) => {
+    if(err) throw err;
+    res.render('index');
+  });
 });
