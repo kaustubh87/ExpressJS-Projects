052b226b4f5a3a1ddced790e7c9bbe9413ca7784
kvin1087 <kaustubh87@gmail.com>
2017-03-17 20:17:30 +0530

    Added configuration files

--- sports-blog/app.js
+++ sports-blog/app.js
@@ -2,3 +2,50 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var expressValidator = require('express-validator');
+var port = 3000;
+var flash = require('connect-flash');
+
+var app = express();
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'pug');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('connect-flash')());
+app.use(function(req,res,next){
+  res.locals.messages = require('express-messages')(req,res);
+  next();
+
+});
+
+//Express Vaidator
+
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+app.get('/', function(req,res){
+  res.send('Index');
+});
+
+app.listen(port, function(req,res){
+  console.log('Server running at ' +port);
+
+});
