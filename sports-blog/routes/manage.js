102ee89d94844a589f27abc3ea5a363eba0b2099
kvin1087 <kaustubh87@gmail.com>
2017-03-19 22:56:47 +0530

    Added more routes

--- sports-blog/routes/manage.js
+++ sports-blog/routes/manage.js
@@ -5,4 +5,12 @@ router.get('/manage', function(req,res){
   res.send('MANAGE');
 });
 
+router.get('/articles/add', function(req,res,next){
+  res.render('add_article', {title: 'Create Article'});
+});
+
+router.get('/categories/add', function(req,res,next){
+  res.render('add_category', {title: 'Create Category'});
+});
+
 module.exports = router;
