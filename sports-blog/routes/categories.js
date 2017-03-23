445b13d9f90453b503013abe3188e3b7e872ee45
kvin1087 <kaustubh87@gmail.com>
2017-03-23 16:32:52 -0500

    Added new route to insert data

--- sports-blog/routes/categories.js
+++ sports-blog/routes/categories.js
@@ -6,9 +6,30 @@ var Category = require('../models/categories.js');
 router.get('/', function(req,res){
   Category.getCategories(function(err, categories){
       console.log(categories);
-      res.render('categories', {title: 'Categories'});
+      if(err){
+        res.send(err);
+      }
+
+      res.render('categories', {
+        title: 'Categories',
+        categories: categories
+      });
   });
 
+});
+
+router.post('/add', function(req,res,next){
+  var category = new Category();
+  category.title = req.body.title;
+  category.description = req.body.description;
+
+  Category.addCategory(category, function(err,category){
+    if(err)
+    {
+      res.send(err);
+    }
+  res.redirect('/manage/categories');
+  });
 });
 
 module.exports = router;
