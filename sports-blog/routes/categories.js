205477ce8397390088adadb38980917dbdd0fc29
kvin1087 <kaustubh87@gmail.com>
2017-03-23 14:21:15 -0500

    Schema for Categories

--- sports-blog/routes/categories.js
+++ sports-blog/routes/categories.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var Category = require('../models/categories.js');
+
 router.get('/', function(req,res){
-  res.render('categories', {title: 'Categories'});
+  Category.getCategories(function(err, categories){
+      console.log(categories);
+      res.render('categories', {title: 'Categories'});
+  });
+
 });
 
 module.exports = router;
