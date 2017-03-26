5ddce683d24e1eefff87b909f1666e7d8d6f0332
kvin1087 <kaustubh87@gmail.com>
2017-03-25 19:59:40 -0500

    Added new routes

--- sports-blog/routes/categories.js
+++ sports-blog/routes/categories.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var Category = require('../models/categories.js');
 
+//Add Category and Post
 router.get('/', function(req,res){
   Category.getCategories(function(err, categories){
       console.log(categories);
@@ -24,6 +25,25 @@ router.post('/add', function(req,res,next){
   category.description = req.body.description;
 
   Category.addCategory(category, function(err,category){
+    if(err)
+    {
+      res.send(err);
+    }
+  res.redirect('/manage/categories');
+  });
+});
+
+//Edit Category and Save Post
+router.post('/edit/:id', function(req,res,next){
+  var category = new Category();
+  var query = {_id: req.params.id};
+  var update = {
+    title: req.body.title,
+    description: req.body.description
+  };
+
+
+  Category.updateCategory(query, update, {}, function(err,category){
     if(err)
     {
       res.send(err);
