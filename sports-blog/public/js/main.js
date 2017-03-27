2e85209ce710f039e98575fe0e965742fc243938
kvin1087 <kaustubh87@gmail.com>
2017-03-26 19:27:57 -0500

    Changed custom js function

--- sports-blog/public/js/main.js
+++ sports-blog/public/js/main.js
@@ -1,9 +1,16 @@
-$(document).ready(function(){
-  $('.delete-category').on('click', function(e){
+$(document).ready(() => {
+  $('.delete-category').on('click', (e) => {
+    $target = $(e.target);
     $.ajax({
       type: 'DELETE',
-      url:'/'
-    })
-
+      url:'/categories/delete/' +$target.attr('data-cat-id'),
+      success: (response) => {
+        alert('Category removed');
+        //window.location.href='/';
+      },
+      error: (error) => {
+        console.log(error);
+      }
+      });
+    });
   });
-});
