d29cdabccb6ef6d2d57fa609dbbedf390303f3b1
kvin1087 <kaustubh87@gmail.com>
2017-03-28 16:16:04 -0500

    Added article route

--- sports-blog/routes/articles.js
+++ sports-blog/routes/articles.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Article = require('../models/Articles.js');
 
 router.get('/', function(req,res){
   res.render('articles', {title: 'Articles'});
@@ -11,6 +12,23 @@ router.get('/show/:id', function(req,res){
 
 router.get('/category/:category_id', function(req,res){
   res.render('articles', {title: 'Category Articles'});
+});
+
+router.post('/articles/add', function(req,res){
+  var article = new Article();
+  article.title = req.body.title;
+  article.subtitle = req.body.subtitle;
+  article.category = req.body.category;
+  article.body = req.body.body;
+  article.author = req.body.author;
+
+  Article.addArticle(article, function(err,article){
+    if(err)
+    {
+      res.send(err);
+    }
+  res.redirect('/manage/articles');
+  });
 });
 
 module.exports = router;
