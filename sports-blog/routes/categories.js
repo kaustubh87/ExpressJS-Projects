d739edfb3d6cca0d21e7a938b3bb864f3ae3dc6d
kvin1087 <kaustubh87@gmail.com>
2017-03-31 16:18:36 -0500

    Added error and success messages

--- sports-blog/routes/categories.js
+++ sports-blog/routes/categories.js
@@ -20,35 +20,62 @@ router.get('/', function(req,res){
 });
 
 router.post('/add', function(req,res,next){
-  var category = new Category();
-  category.title = req.body.title;
-  category.description = req.body.description;
+  req.checkBody('title', 'Title is required').notEmpty();
+  req.checkBody('description', 'Description is required').notEmpty();
+  var errors = req.validationErrors();
+  if(errors){
+    res.render('add_category', {
+      errors: errors,
+      title: 'Create Category'
+    });
+  }
+  else{
+    var category = new Category();
+    category.title = req.body.title;
+    category.description = req.body.description;
 
-  Category.addCategory(category, function(err,category){
-    if(err)
-    {
-      res.send(err);
-    }
-  res.redirect('/manage/categories');
-  });
+    Category.addCategory(category, function(err,category){
+      if(err)
+      {
+        res.send(err);
+      }
+    req.flash('success', 'Category Saved');
+    res.redirect('/manage/categories');
+    });
+  }
 });
 
 //Edit Category and Save Post
 router.post('/edit/:id', function(req,res,next){
-  var category = new Category();
-  var query = {_id: req.params.id};
-  var update = {
-    title: req.body.title,
-    description: req.body.description
-  };
+  req.checkBody('title', 'Title is required').notEmpty();
+  req.checkBody('description', 'Description is required').notEmpty();
+  var errors = req.validationErrors();
 
-  Category.updateCategory(query, update, {}, function(err,category){
-    if(err)
-    {
-      res.send(err);
-    }
-  res.redirect('/manage/categories');
-  });
+  if(errors){
+    res.render('edit_category', {
+      error: errors,
+      title : 'Create Category'
+    });
+  }
+  else {
+    var category = new Category();
+    var query = {_id: req.params.id};
+    var update = {
+      title: req.body.title,
+      description: req.body.description
+    };
+
+    Category.updateCategory(query, update, {}, function(err,category){
+      if(err)
+      {
+        res.send(err);
+      }
+    req.flash('success', 'Category Updated');
+    res.redirect('/manage/categories');
+    });
+  }
+
+
 });
 
 //Delete Category
