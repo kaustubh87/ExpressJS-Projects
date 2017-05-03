8563ced36a456b59fc79dfa41c49ffe703fdb72c
kvin1087 <kaustubh87@gmail.com>
2017-05-03 09:55:20 -0500

    User login system

---
+++ user-login-system/app.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var mongoose = require('mongoose');
+var port = process.env.PORT || 3000;
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Express Session
+
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+//Express messages
+
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+
+
+//Express Validator from github
+
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+app.use('/', index);
+app.use('/users', users);
+
+app.listen(process.env.PORT || )
