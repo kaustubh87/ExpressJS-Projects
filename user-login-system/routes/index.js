20a67c58a38850ff670195cf3f5c8e86773fe34d
kvin1087 <kaustubh87@gmail.com>
2017-05-04 16:13:43 -0500

    Final tasks added to make the project complete

--- user-login-system/routes/index.js
+++ user-login-system/routes/index.js
@@ -6,10 +6,9 @@ var bcrypt = require('bcryptjs');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-router.get('/', function(req,res,next){
+router.get('/', ensureAuthenticated, function(req,res,next){
    res.render('index'); 
 });
-
 
 router.get('/register', function(req,res,next){
    res.render('register');
@@ -17,6 +16,17 @@ router.get('/register', function(req,res,next){
 
 router.get('/login', function(req,res,next){
    res.render('login');
+});
+
+
+router.get('/home', function(req,res,next){
+   res.render('home'); 
+});
+
+router.get('/logout', function(req,res,next){
+   req.logout();
+   req.flash('success_msg', 'You are logged out');
+   res.redirect('/login');
 });
 
 router.post('/register', function(req,res,next){
@@ -53,44 +63,35 @@ router.post('/register', function(req,res,next){
   
 });
 
-
 //Local Strategy
 
-passport.use(new LocalStrategy(function(username, password, done){
-    User.getUserByUsername(username, function(err, user){
-        if(err){
-            throw err;
-        }
-        if(!user){
-            return done(null, false,{message:'User not found'} );
-        }
-        
-        User.comparePassword(password, user.password, function(err, isMatch){
-           if(err) throw err;
-            if(isMatch){
-                return done(null, user);
-            }
-            else
-                {
-                    return done(null, false,{message: 'Wrong Password'});
-                }
-        });
-        
+// Local Strategy
+passport.use(new LocalStrategy((username, password, done) => {
+  User.getUserByUsername(username, function(err, user){
+    if(err) throw err;
+    if(!user){
+      return done(null, false, {message: 'No user found'});
+    }
+
+    User.comparePassword(password, user.password, function(err, isMatch) {
+      if(err) throw err;
+      if(isMatch){
+        return done(null, user);
+      } else {
+        return done(null, false, {message: 'Wrong Password'});
+      }
     });
-    
+  });
 }));
 
-
-//Serializing and Deserializing Users.
-
-passport.serializeUser(function(user, done){
-    done(null, user.id);
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done){
-   User.getUserById(id, function(err, user){
-    done (err, user);
-   });
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
 });
 
 
@@ -98,12 +99,23 @@ passport.deserializeUser(function(id, done){
 
 router.post('/login', function(req,res,next){
     passport.authenticate('local', {
-       successRedirect : '/',
+       successRedirect : '/home',
        failureRedirect : '/login',
        failureFlash : true
-    }, function(req,res){
-        res.redirect('/');
-    });
+    })(req, res, next);
 });
+
+
+//Access Control
+
+function ensureAuthenticated(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    else{
+        req.flash('error_msg', 'You are not authorized to view that page');
+        res.redirect('/login');
+    }
+}
 
 module.exports = router;
