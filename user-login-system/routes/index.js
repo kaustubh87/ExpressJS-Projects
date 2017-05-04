6563169b64662ab414bc47e1d8f037176e65e204
kvin1087 <kaustubh87@gmail.com>
2017-05-04 08:16:19 -0500

    Added functions to check whether user exists and the password is correct

--- user-login-system/routes/index.js
+++ user-login-system/routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user');
+var bcrypt = require('bcryptjs');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 router.get('/', function(req,res,next){
    res.render('index'); 
 });
@@ -8,6 +13,10 @@ router.get('/', function(req,res,next){
 
 router.get('/register', function(req,res,next){
    res.render('register');
+});
+
+router.get('/login', function(req,res,next){
+   res.render('login');
 });
 
 router.post('/register', function(req,res,next){
@@ -23,19 +32,70 @@ router.post('/register', function(req,res,next){
     req.checkBody('username' , 'Username is required').notEmpty();
     req.checkBody('password' , 'Password is required').notEmpty();
     req.checkBody('password2' , 'Passwords do not match').equals(req.body.password);
-    
-    var errors = req.validateErrors();
-    if(errors){
-        res.render('register',{
-            errors: errors
-        });
-    }
-    else
-        {
-            console.log('Success');
-            return;
-        }
   
+            var newUser = new User({
+                name: name,
+                username: username,
+                email: email,
+                password: password
+            });
+            
+            User.registerUser(newUser, function(err, user){
+                if(err){
+                    throw err;
+                }
+                else{
+                    req.flash('success_msg', 'You are registered and can login');
+                    res.redirect('/login');
+                }
+            });
+        
+  
+});
+
+
+//Local Strategy
+
+passport.use(new LocalStrategy(function(username, password, done){
+    User.getUserByUsername(username, function(err, user){
+        if(err){
+            throw err;
+        }
+        if(!user){
+            return done(null, false,{message:'User not found'} );
+        }
+        
+        User.comparePassword(password, user.password, function(err, isMatch){
+           if(err) throw err;
+            if(isMatch){
+                return done(null, user);
+            }
+            else
+                {
+                    return done(null, false,{message: 'Wrong Password'});
+                }
+        });
+        
+    });
+    
+}));
+
+
+//Serializing and Deserializing Users.
+
+
+
+
+//Login
+
+router.post('/login', function(req,res,next){
+    passport.authenticate('local', {
+       successRedirect : '/',
+       failureRedirect : '/login',
+       failureFlash : true
+    }, function(req,res){
+        res.redirect('/');
+    });
 });
 
 module.exports = router;
