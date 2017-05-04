c28575382238cf9eb7131693f0bc82371adbe905
kvin1087 <kaustubh87@gmail.com>
2017-05-04 08:27:08 -0500

    Added login route

--- user-login-system/routes/index.js
+++ user-login-system/routes/index.js
@@ -83,7 +83,15 @@ passport.use(new LocalStrategy(function(username, password, done){
 
 //Serializing and Deserializing Users.
 
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
 
+passport.deserializeUser(function(id, done){
+   User.getUserById(id, function(err, user){
+    done (err, user);
+   });
+});
 
 
 //Login
