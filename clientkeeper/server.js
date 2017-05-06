545cbc5f34fd178bc22757a12493b42b0d6b0267
kvin1087 <kaustubh87@gmail.com>
2017-05-05 21:37:59 -0500

    Added modules

--- clientkeeper/server.js
+++ clientkeeper/server.js
@@ -2,10 +2,21 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-
+var port = process.env.port || 3000;
 var app = express();
 var mongojs = require('mongojs');
 
 var db = mongojs('clientkeeper', ['clients']);
 
-app.use(express.static(__dirname , 'public'));
+app.use(express.static(path.join(__dirname , 'public')));
+app.use(bodyParser.json());
+
+app.get('/', function(req,res){
+    res.send(' Please use /api/clients as url');
+});
+
+app.listen(port, function(req,res){
+    console.log('Server started on '+port);
+});
+
+
