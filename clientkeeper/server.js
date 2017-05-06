68ec47f85210d43565bfb1721e1b7af7319c3f33
kvin1087 <kaustubh87@gmail.com>
2017-05-06 09:57:29 -0500

    Added get and post requests

--- clientkeeper/server.js
+++ clientkeeper/server.js
@@ -15,6 +15,28 @@ app.get('/', function(req,res){
     res.send(' Please use /api/clients as url');
 });
 
+app.get('/api/clients' , function(req,res){
+   db.clients.find().sort({first_name: 1}, function(err,clients){
+       if(err){
+           res.send(err);
+       }
+       else{
+           res.json(clients);
+       }
+       
+   });
+});
+
+
+app.post('/api/clients' , function(req,res){
+   db.clients.insert(req.body, function(err, client){
+       if(err){
+           res.send(err);
+       }
+       res.json(client);
+   }); 
+});
+
 app.listen(port, function(req,res){
     console.log('Server started on '+port);
 });
