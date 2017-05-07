258d89ae96608743ca4eba19d2ef4a0b4c484c0e
kvin1087 <kaustubh87@gmail.com>
2017-05-07 18:25:13 -0500

    Installed modules

--- clientkeeper/server.js
+++ clientkeeper/server.js
@@ -37,6 +37,45 @@ app.post('/api/clients' , function(req,res){
    }); 
 });
 
+//PUT request
+
+app.put('/api/clients/:id' , function(req,res){
+    
+    var id = req.params.id;
+    
+   db.clients.findAndModify({query: {_id : mongojs.ObjectId(id)},
+       update: {
+        $set : {
+        first_name : req.body.first_name,
+        last_name : req.body.last_name,
+        email: req.body.email,
+        phone: req.body.phone
+        }},
+       new: true},
+        function(err, client){
+    if(err)
+        {
+        res.json(err);
+        }
+    else
+        res.json(client);
+    
+});
+});
+
+
+//Delete request
+
+app.delete('/api/clients/:id', function(req,res){
+   var id = req.params.id;
+    db.clients.remove({_id: mongojs.ObjectId(id)},function(err, client){
+       if(err){
+           res.send(err);
+       }
+        res.json(client);
+    });
+});
+
 app.listen(port, function(req,res){
     console.log('Server started on '+port);
 });
