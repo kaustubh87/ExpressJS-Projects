945321706b3a7b56d01f5f22f7e6c1f7bfde838a
kvin1087 <kaustubh87@gmail.com>
2017-05-11 10:22:22 -0500

    Added views

---
+++ MovieBase/controllers/movies.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var Movie = require('../models/movies');
+
+
+module.exports = function (router) {
+
+    var model = new Movie();
+    
+    //Movie Listing
+    router.get('/', function (req, res) {
+        
+        Movie.find({}, function(err, movies){
+           if(err) res.send(err);
+            
+            var model = {
+                movies: movies
+            }
+            
+        res.render('movies', model);
+            
+        });
+        
+        
+        
+        
+    });
+    
+    
+    
+    
+
+};
