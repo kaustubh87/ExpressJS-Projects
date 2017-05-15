03185414210a0b3d706f853c8429569f1ddb1b17
kvin1087 <kaustubh87@gmail.com>
2017-05-14 21:32:25 -0500

    Added js to delete a movie

--- MovieBase/public/js/app.js
+++ MovieBase/public/js/app.js
@@ -1,20 +1,22 @@
-/*global requirejs:true*/
-'use strict';
-
-
-requirejs.config({
-    paths: {}
-});
-
-
-require([/* Dependencies */], function () {
-
-    var app = {
-        initialize: function () {
-            // Your code here
-        }
-    };
-
-    app.initialize();
-
-});
+$(document).ready(function(){
+   
+    $('.delete-movie').click(function(event){
+       $target = $(event.target);
+        $.ajax({
+            type: 'DELETE',
+            url: '/movies/delete' + $target.attr('data-movie-id'),
+            success: function(response){
+                $target.parent().parent().remove();
+                
+                window.location.href='/movies';
+                
+            },
+            error: function(error){
+                alert(error);
+                console.log(error);
+            }
+        })
+        
+    });
+    
+})
